660e1458cc4274f6d1acf84a39b575702f8ca00d
newcho913 <newcho913@gmail.com>
2014-06-02 16:45:02 +0900

    popup

--- src/kr/re/ec/bibim/ui/LoginFrame.java
+++ src/kr/re/ec/bibim/ui/LoginFrame.java
@@ -4,14 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class LoginFrame extends AbstractFrame {
+public class LoginFrame implements ActionListener {
 	private boolean topview = true;
-	 private Container con;
-	 private JTextField tf1 = new JTextField(10);  //아이디를 쓸 텍스트 필드
-	 private JPasswordField tf2 = new JPasswordField (10);  //비밀번호를 쓸 패스워드 필드
-	 private JButton bt1 = new JButton("등록");
-	 private JButton bt2 = new JButton("확인");
-	
+	private Container con;
+	private JTextField idtextfield = new JTextField(10); // 아이디를 쓸 텍스트 필드
+	private JPasswordField pwdtextfield = new JPasswordField(10); // 비밀번호를 쓸
+																	// 패스워드 필드
+	private JButton signupbt = new JButton("가입");
+	private JButton loginbt = new JButton("확인");
+
 	public void init(){
 		JFrame jf = new JFrame();	
 		jf.setSize(500,300);
@@ -22,7 +23,6 @@ public class LoginFrame extends AbstractFrame {
 		
 		//전체 창 사이즈 가져오는거네
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		System.out.println(screenSize.width+","+screenSize.height);
 
 		//컴퓨터 화면 크기가 다르지만 모든 컴퓨터의 화면에 -- 창을 중앙에 위치시킬 수 있다.
 		int xPos = screenSize.width/2 - jf.getSize().width/2;
@@ -31,27 +31,40 @@ public class LoginFrame extends AbstractFrame {
 		jf.setLocation(xPos,yPos);
 		jf.setResizable(false);
 		
-		jf.addWindowListener(new WindowAdapter() {
+	/*	jf.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(1);
 			}
-		});	
+		});	 */
 		
 		// 화면구성
 		con = jf.getContentPane();
 		JPanel pn1 = new JPanel(new FlowLayout());
 		pn1.add(new JLabel("ID"));
-		pn1.add(tf1);
+		pn1.add(idtextfield);
 		JPanel pn2 = new JPanel(new FlowLayout());
-		pn2.add(new JLabel("Password"));
-		pn2.add(tf2);
+		pn2.add(new JLabel("Passward"));
+		pn2.add(pwdtextfield);
 		JPanel pn3 = new JPanel(new FlowLayout());
-		pn3.add(bt1);
-		pn3.add(bt2);
+		pn3.add(signupbt);
+		pn3.add(loginbt);
 		con.setLayout(new GridLayout(3, 1));
 		con.add(pn1);
 		con.add(pn2);
 		con.add(pn3);
 		
+		
+		signupbt.addActionListener(this);
+		loginbt.addActionListener(this);
+	
+	
 	}
+	
+	public void actionPerformed(ActionEvent arg0) {
+    // TODO Auto-generated method stub
+    if(arg0.getSource() == signupbt || arg0.getSource() == loginbt){
+            new PopupFrame(arg0.getActionCommand() + " 버튼을 누르셨군요!");
+    }
+}
+
 }

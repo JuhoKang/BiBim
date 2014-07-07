7c58a8472b27dc13ff6787560fd84c3a6391ceb2
Kang Juho <rjduakcjs@gmail.com>
2014-07-07 17:36:49 +0900

    edited ignore. added slf4j for logging

--- src/kr/re/ec/bibim/Main.java
+++ src/kr/re/ec/bibim/Main.java
@@ -1,12 +1,19 @@
 package kr.re.ec.bibim;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import kr.re.ec.bibim.controller.LoginController;
 
 
 public class Main {
 	public static void main(String[] args) {
 //changed something new LoginFrame().init();
-
+		Logger logger = LoggerFactory.getLogger(Main.class);
+		logger.info("hello!");
+		logger.info(""+logger.isDebugEnabled());
+		logger.debug("shit");
 		new LoginController().init();
 		//new SignupFrame().init();
 
